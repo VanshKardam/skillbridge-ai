f6fa853fdad47519966016dbe25f6f392b802ea9
Vanshkardam <vanshkardam36@gmail.com>
2026-06-21 23:21:32 +0530

    feat: implement user registration and login authentication using Express, Mongoose, and JWT

--- src/app.js
+++ src/app.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express()
 
+// Middleware for parsing JSON
 app.use(express.json())
+
+// Import and use auth routes
+const authRouter = require("./routes/auth.routes")
+
+// Using all the routes here
+app.use("/api/auth", authRouter)
 
 module.exports = app
