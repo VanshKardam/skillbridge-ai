e0b74d4697210cf3746e4aa6a8a843c918a709a6
Vanshkardam <vanshkardam36@gmail.com>
2026-06-23 17:56:09 +0530

    feat: create blacklist token schema and model for authentication revocation

--- Backend/src/models/blacklist.model.js
+++ Backend/src/models/blacklist.model.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose")
 
 const blacklistTokenSchema = new mongoose.Schema({
-    token: String,
-    required: [true, "token is required to be added in blacklist"]
+    token: {
+        type: String,
+        required: [true, "token is required to be added in blacklist"]
+    }
 }, {
     timestamps: true
 })
